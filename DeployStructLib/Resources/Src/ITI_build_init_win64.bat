302d8fdd427ddcd0380cf5a882087e46166c5eb7
Thomas Beutlich <thomas.beutlich@esi-group.com>
2018-11-20 08:02:20 +0100

    Update ITI_cloth_init.dll

--- DeployStructLib/Resources/Src/ITI_build_init_win64.bat
+++ DeployStructLib/Resources/Src/ITI_build_init_win64.bat
@@ -10,4 +10,3 @@ gcc -c VectorCalcs.c -m64 -O2
 gcc -m64 -o ITI_cloth_init.dll .\cloth_init.o .\VectorCalcs.o -shared -L . -l"ModelicaExternalC"
 DEL cloth_init.o VectorCalcs.o
 MOVE ITI_cloth_init.dll ..\Library\win64\ >nul 2>&1
-
